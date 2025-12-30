dd1d5f262c4b7bcb90ceb4ee3038381f3d49d7ff
hub-jogos-entre-amigos <hubjogos.entre.amigos@gmail.com>
2025-12-29 23:54:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,10 @@
-const CACHE_NAME = 'jea-hub-v6';
-const assets = [
-  'index.html',
-  'manifest.json',
-  'sw.js',
-  'icon-192.png',
-  'icon-512.png',
-  'letras.html',
-  'conhece.html',
-  'imperium.html',
-  'nem-pensar.html'
-];
+const CACHE_NAME = 'jea-hub-v7';
+const assets = ['index.html', 'manifest.json', 'icon-192.png'];
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Força a ativação imediata
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
